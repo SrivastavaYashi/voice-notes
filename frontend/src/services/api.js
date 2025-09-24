f722ea4c05bab3a8613e0833a7a2cd95c1f22049
yashi srivastava <coder7577@gmail.com>
2025-09-24 20:58:59 +0530

    Backend URL Changed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL||"https://voice-notes-backend-5682.onrender.com";
+const API_BASE_URL = import.meta.env.VITE_API_URL||"https://voice-notes-backend-5682.onrender.com/api/notes";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
